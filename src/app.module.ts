6c74ddef0ccf94b6da9974e91fe353e57e515e87
AEM version script <you@example.com>
2021-10-18 21:27:10 +0300

    connect mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { ArticlesModule } from './articles/articles.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticleEntity } from './articles/article.entity';
 
 @Module({
     imports: [
+        TypeOrmModule.forRoot({
+            type: 'mongodb',
+            url: 'mongodb://localhost/nest-blog',
+            synchronize: true,
+            useUnifiedTopology: true,
+            entities: [ArticleEntity],
+        }),
         MongooseModule.forRoot('mongodb://localhost/nest-blog'),
         GraphQLModule.forRoot({
             autoSchemaFile: true,
